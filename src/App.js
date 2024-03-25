68247a155fd6e69f81b73981f00c6bf027819344
Shubham Patil <shubham.patil@netcracker.com>
2024-03-25 23:35:40 +0530

    Added create wallet form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/common/Nav';
 import Welcome from './components/Welcome';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/dashboard/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import WalletForm from './components/dashboard/operations/WalletForm';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Welcome />} />
         <Route exact path='/dashboard' element={<Dashboard />} />
+        <Route exact path='/createwallet' element={<WalletForm />} />
       </Routes>
     </BrowserRouter>
   );
