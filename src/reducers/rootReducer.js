32cccb606c416445abfc7362f70812e0693a0213
Shubham Patil <shubham.patil@netcracker.com>
2024-04-19 09:55:28 +0530

    View Transactions

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import errorReducer from './errorReducer';
 import walletReducer from './walletReducer';
+import transactionReducer from './transactionReducer';
 
 const rootReducer = combineReducers({
   errors: errorReducer,
   wallet: walletReducer,
+  transaction: transactionReducer,
 });
 
 export default rootReducer;
